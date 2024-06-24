65c9beb6b2d4bf5f20717faff195cba24d39d71a
Ben <blearn5241@gmail.com>
2024-06-24 02:57:43 -0400

    added get function to callout util from warehouse

--- util/callout.js
+++ util/callout.js
@@ -1,3 +1,21 @@
+const axios = require('axios');
+
+const { API_BASES, REQUEST_HEADERS } = require('../config');
+const { fatal } = require('./error.js');
+
+async function get({site, endpoint, fullUrl}) {
+    try {
+        const res = await axios.get(
+            fullUrl ? fullUrl : `${API_BASES[site]}${endpoint}`,
+            {
+                headers: REQUEST_HEADERS[site]
+            },
+        );
+        return res.data;
+    } catch(err) {
+        fatal('get()', err.message);
+    }
+}
 
 async function patch(site, endpoint, body) {
     try {
@@ -45,6 +63,7 @@ async function put(site, endpoint, body) {
 }
 
 module.exports = {
+    get,
     patch,
     post,
     put
