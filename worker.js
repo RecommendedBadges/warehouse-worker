636645b4f44d575f226acc1a60ae6fd3578fffe1
Ben <blearn5241@gmail.com>
2024-06-29 16:00:03 -0400

    removed test code

--- worker.js
+++ worker.js
@@ -11,7 +11,6 @@ async function start() {
   let workQueue = new Queue('work', process.env.REDIS_URL);
   
   process.stdout.write('Worker started.\n');
-  await cloneRepo(285);
 
   workQueue.process('kickoff', async (job) => {
     process.stdout.write('Kickoff job received\n');
