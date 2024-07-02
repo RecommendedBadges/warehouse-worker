35529063744e48755316b932733b72535fa47ac8
Ben <blearn5241@gmail.com>
2024-07-01 21:00:06 -0400

    installing honeybadger

--- worker.js
+++ worker.js
@@ -1,14 +1,23 @@
 const { orchestrate, setupScheduledJob } = require('./services');
 const { post } = require('./util');
 
+const Honeybadger = require("@honeybadger-io/js");
+Honeybadger.configure({
+  apiKey: process.env.HONEYBADGER_API_KEY,
+  environment: "production"
+});
+
 let throng = require('throng');
 let Queue = require("bull");
+
 
 let workers = process.env.WEB_CONCURRENCY || 1;
 
 async function start() {
   // Connect to the named work queue
   let workQueue = new Queue('work', process.env.REDIS_URL);
+  Honeybadger.notify("Testing Honeybadger!");
+
   
   process.stdout.write('Worker started.\n');
 
