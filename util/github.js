df3c9f12f2e47393c82aaab08e523d7c47a1ff17
Ben <blearn5241@gmail.com>
2024-07-06 12:00:56 -0400

    readding async/awaitto github

--- util/github.js
+++ util/github.js
@@ -1,24 +1,24 @@
 const callout = require('./callout.js');
 const { BASE_BRANCH, PACKAGES_LABEL } = require('../config');
 
-function commentOnPullRequest(pullRequestNumber, commentBody) {
-    callout.post('github', `/issues/${pullRequestNumber}/comments`, {body: commentBody});
+async function commentOnPullRequest(pullRequestNumber, commentBody) {
+    await callout.post('github', `/issues/${pullRequestNumber}/comments`, {body: commentBody});
 }
 
-function getIssueComments(issueNumber) {
-    let issueComments = callout.get({
+async function getIssueComments(issueNumber) {
+    let issueComments = await callout.get({
         site: 'github',
         endpoint: `/issues/${issueNumber}/comments`
     });
     return issueComments;
 }
 
-function getOpenPullRequestDetails(parameters) {
-    let pullRequests = callout.get({
+async function getOpenPullRequestDetails(parameters) {
+    let pullRequests = await callout.get({
         site: 'github',
         endpoint: '/pulls'
     });
-    process.stdout.write(`${pullRequests}`);
+
     for(let pullRequest of pullRequests) {
         if(
             (pullRequest.base.ref === BASE_BRANCH) 
@@ -29,12 +29,12 @@ function getOpenPullRequestDetails(parameters) {
     }
 }
 
-function deletePackageLabelFromIssue(issueNumber) {
-    callout.doDelete('github', `/issues/${issueNumber}/labels/${PACKAGES_LABEL}`);
+async function deletePackageLabelFromIssue(issueNumber) {
+    await callout.doDelete('github', `/issues/${issueNumber}/labels/${PACKAGES_LABEL}`);
 }
 
-function mergeOpenPullRequest(pullRequestNumber) {
-    callout.put('github', `/pulls/${pullRequestNumber}/merge`);
+async function mergeOpenPullRequest(pullRequestNumber) {
+    await callout.put('github', `/pulls/${pullRequestNumber}/merge`);
 }
 
 module.exports = {
