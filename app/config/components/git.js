3e4b40368696dd9bd7c6750f2d274df10d930ca2
Ben <blearn5241@gmail.com>
2024-08-17 18:06:36 -0400

    updating git commit command w/ author specification

--- app/config/components/git.js
+++ app/config/components/git.js
@@ -2,7 +2,7 @@ const COMMIT_MESSAGE = 'Updating package.json';
 
 const GIT_CHECKOUT_COMMAND = 'git checkout -f -q';
 const GIT_CLONE_COMMAND = 'git clone';
-const GIT_COMMIT_COMMAND = `git commit -a -m "${COMMIT_MESSAGE}"`;
+const GIT_COMMIT_COMMAND = `git commit -a -m "${COMMIT_MESSAGE}" --author="${process.env.GIT_COMMIT_NAME} ${process.env.GIT_COMMIT_EMAIL}"`;
 const GIT_PULL_COMMAND = 'git pull -q';
 const GIT_PUSH_COMMAND = 'git push';
 
